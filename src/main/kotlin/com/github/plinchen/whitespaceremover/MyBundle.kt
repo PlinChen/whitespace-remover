8922f32f81f5578fc23cb7b0aadc0cc5ec4d273e
GitHub Action <action@github.com>
2025-04-09 06:43:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/plinchen/whitespaceremover/MyBundle.kt
+++ src/main/kotlin/com/github/plinchen/whitespaceremover/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.plinchen.whitespaceremover
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/plinchen/whitespaceremover/services/MyProjectService.kt
+++ src/main/kotlin/com/github/plinchen/whitespaceremover/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.plinchen.whitespaceremover.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.plinchen.whitespaceremover.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
